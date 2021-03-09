a80b52a1ad016cda8b9c5625d91bc592025fb55a
maestro888 <maestro888ua@gmail.com>
2021-03-09 22:59:32 +0200

    Changed default color in variables.

--- src/js/table-fluid.js
+++ src/js/table-fluid.js
@@ -1,6 +1,6 @@
 /**
  * --------------------------------------------------------------------------
- * Responsive Table (v1.1.1): table-fluid.js
+ * Responsive Table (v1.1.2): table-fluid.js
  * Copyright (c) 2021 maestro888
  * Licensed under MIT (https://github.com/maestro888/table-fluid/blob/master/LICENSE)
  * --------------------------------------------------------------------------
